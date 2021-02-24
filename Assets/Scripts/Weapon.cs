c43387c9dd20e18eb3d49b142a4390144b033221
Fish_In_A_Suit <skorjanc.aljosa@gmail.com>
2021-02-24 17:13:09 +0100

    0.1.0: Raycast shooting support
    Added a second player character as a prefab, which shoots based on raycasts, giving much nicer collision detection.
    
    When swapping the two player character prefabs, it is important to drag the play-body to the Camera Follow variable of the main camera (and also to patch any missing camera fields in player components)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -12,18 +12,16 @@ public class Weapon : MonoBehaviour
     Vector2 mousePos;
     void Update()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 firePointVec2 = new Vector2(firePoint.position.x, firePoint.position.y);
-        Vector2 projectileDirection = mousePos - firePointVec2;
-        projectileDirection.Normalize();
-
-        float angle = Mathf.Atan2(projectileDirection.y, projectileDirection.x) * Mathf.Rad2Deg - 90f; //angle = z rotation
-        firePoint.rotation = Quaternion.Euler(0, 0, angle);
-        //Debug.Log("angle = " + angle);
-        //Debug.Log("firepoint.rotation = " + firePoint.rotation);
-
         if (Input.GetMouseButtonDown(1))
         {
+            mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 firePointVec2 = new Vector2(firePoint.position.x, firePoint.position.y);
+            Vector2 projectileDirection = mousePos - firePointVec2;
+            projectileDirection.Normalize();
+
+            float angle = Mathf.Atan2(projectileDirection.y, projectileDirection.x) * Mathf.Rad2Deg - 90f; //angle = z rotation
+            firePoint.rotation = Quaternion.Euler(0, 0, angle);
+
             GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
             bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(projectileDirection.x * speed, projectileDirection.y*speed); 
         }
---
+++ Assets/Scripts/WeaponRaycast.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script is an alternative to the Weapon.cs script. Weapon.cs uses prefab-based shooting, whereas this script uses Raycast based shooting.
+ * 
+ * */
+public class WeaponRaycast : MonoBehaviour
+{
+
+    public Transform firePoint; //the point where player is firing from
+    public Camera cam; //main camera
+    public int damage = 20; //bullet damage
+    public float projectileDisplayDuration = 0.02f; //the amount of time the projectile line will be rendered
+    public LayerMask colliderMask; //layers set in colliderMask will count as valid collisions
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer; //line renderer of the projectile's line
+
+    Vector2 mousePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            StartCoroutine(Shoot());
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        Vector3 mouse = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 shootDirection = mouse - firePoint.position;
+
+        Debug.DrawRay(firePoint.position, shootDirection, Color.red, 5, false);
+        RaycastHit2D hitInfo = Physics2D.Raycast(firePoint.position, shootDirection, Mathf.Infinity, colliderMask);
+        
+
+        if (hitInfo)
+        {
+            Enemy enemy = hitInfo.transform.GetComponent<Enemy>();
+            Debug.Log("name = " + hitInfo.transform.name);
+            Debug.Log("enemy = " + enemy);
+            if(enemy != null)
+            {
+                enemy.TakeDamage(20);
+            }
+
+            Instantiate(impactEffect, hitInfo.point, Quaternion.identity);
+
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, hitInfo.point);
+        } else {
+            lineRenderer.SetPosition(0, firePoint.position);
+            lineRenderer.SetPosition(1, firePoint.position + shootDirection * 100);
+        }
+
+        //coroutines enable stops in code
+        lineRenderer.enabled = true;
+        yield return new WaitForSeconds(projectileDisplayDuration);
+        lineRenderer.enabled = false;
+    }
+}
