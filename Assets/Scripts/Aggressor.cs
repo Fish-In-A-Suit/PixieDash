ad5b616d6a6949bfd6a780d9c24cf26ee931110f
Fish_In_A_Suit <skorjanc.aljosa@gmail.com>
2021-04-01 19:02:36 +0200

    0.4.0 Enemy line of sight
    Added enemy sight using raycasts

---
+++ Assets/Scripts/Aggressor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this script should be attached to enemies. handles firing at the player
+public class Aggressor : MonoBehaviour
+{
+    Transform player;
+    public LayerMask raycastIgnoreLayer; //raycast should ignore the enemy!
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find player
+        player = GameObject.Find("player-body1").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isPlayerVisible();
+    }
+
+    bool isPlayerVisible()
+    {
+        Vector2 rayDirection = player.position - transform.position;
+        Debug.DrawLine(player.position, transform.position, Color.green, 2, false); //enable gizmos if line isn't drawn
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, rayDirection, 1000, ~raycastIgnoreLayer); //~ inverts the layer mask!
+        Debug.Log(rayHit.collider.name);
+        if(rayHit.collider != null)
+        {
+            if(rayHit.collider.name.Equals("player-body1")) {
+                Debug.Log("player visible");
+                return true;
+            } else
+            {
+                Debug.Log("player not visible");
+                return false;
+            }
+        } else
+        {
+            Debug.Log("player not visible");
+            return false;
+        }
+    }
+}
